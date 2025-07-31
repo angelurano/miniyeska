1d156aef1f6f77ee14ea8047ef3f78603436e105
g01100001 <gomandam@student.42madrid.com>
2025-07-31 02:11:52 +0200

    [update] cwd

--- source/built-ins/pwd.c
+++ source/built-ins/pwd.c
@@ -6,27 +6,30 @@
 /*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 02:03:55 by gomandam          #+#    #+#             */
-/*   Updated: 2025/07/25 03:04:43 by gomandam         ###   ########.fr       */
+/*   Updated: 2025/07/31 02:10:34 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	pwd(own_struct	*shell) // alternative prototype: pwd(void)
+int	pwd(void)	// alternative prototype: state of session
 {
 	char *cwd;
 
 	cwd = getcwd(NULL, 0);
-
-	// error checking
 	if (cwd == NULL)
 	{
-		perror("minishell: pwd: error");
-		return (error_code);
+		perror("minishell: pwd: error retrieving current directory");
+		return (0);
 	}
-	printf("CURRENT WORKING DIRECTORY: / / /");
+	printf("%s\n", cwd);	// current working directory
 	free(cwd);
 	return (1);
 }
 
 // _getcwd	https://www.youtube.com/watch?v=n2iSn6zGB5A
+
+/*
+	getcwd() allocate buffer for current dir., check if null
+	perror() error
+*/
