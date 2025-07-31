ea3c2d5418b5cc54dcdb4a94e9bc7198f76eab93
g01100001 <gomandam@student.42madrid.com>
2025-07-31 02:48:23 +0200

    [add] env.c

--- source/built-ins/env.c
+++ source/built-ins/env.c
@@ -6,8 +6,30 @@
 /*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 02:03:25 by gomandam          #+#    #+#             */
-/*   Updated: 2025/07/24 02:03:28 by gomandam         ###   ########.fr       */
+/*   Updated: 2025/07/31 02:47:49 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// protoyped: char *envp[] or struct state t_minishell *shell
+#include "minishell.h"
 
+int	env(char *envp[])
+{
+	int	i;
+
+	i = 0;
+	while (envp && envp[i])		// check not NULL, iterates until NULL
+	{
+		if (ft_strchr(envp[i], '='))	// check '='
+			printf("%s\n", envp[i]);	// std-out
+		i++;				// next env array
+	}
+	return (0);
+}
+
+/*
+	env - lists environmental variables outputs KEY=VALUE by child-process
+	stores info used by processes (e.g. PATH, HOME, USER)
+	each string has "=" so validate on this
+	read (RTFM!) -> man 3 getenv
+*/
