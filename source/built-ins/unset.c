a8a0cc864932dda140f8e2787cec00af53c3130f
g01100001 <gomandam@student.42madrid.com>
2025-08-05 04:37:36 +0200

    [add] unset.c concept

--- source/built-ins/unset.c
+++ source/built-ins/unset.c
@@ -6,15 +6,27 @@
 /*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 02:04:11 by gomandam          #+#    #+#             */
-/*   Updated: 2025/07/25 03:09:17 by gomandam         ###   ########.fr       */
+/*   Updated: 2025/08/05 04:33:56 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	unset(own_struct *placeholder)
-{
-	char	*argv[];
-	char	*aux;
-	int	index[];
+/*
+	unset values & attribs of variables/functions
+	remove access from current shell session, do not affect child process
+*/
 
+#include "../../include/minishell.h"
+
+int	unset(char *argv[], char *envp[])
+{
+	int	a;
+	int	b;
+	char	*var;
+
+	i = 0;
+	if (!argv[0])
+		return (0);
 
 }
+
+// Reference:	export(); function form my export.c
