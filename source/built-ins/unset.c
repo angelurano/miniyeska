cc086b2a58d835a042aa29496ed91b7e82a8d353
g01100001 <gomandam@student.42madrid.com>
2025-08-07 07:02:53 +0200

    [add] unset.c

--- source/built-ins/unset.c
+++ source/built-ins/unset.c
@@ -6,7 +6,7 @@
 /*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 02:04:11 by gomandam          #+#    #+#             */
-/*   Updated: 2025/08/05 04:33:56 by gomandam         ###   ########.fr       */
+/*   Updated: 2025/08/07 02:15:31 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,24 @@ int	unset(char *argv[], char *envp[])
 	i = 0;
 	if (!argv[0])
 		return (0);
-
+	var = argv[1];
+	while (envp[a])
+	{
+		if (ft_strncmp(envp[a], var, ft_strlen(var)) == 0
+			&& envp[a][ft_strlen(var) == '='])
+		{
+			b = a;
+			while (envp[b])
+			{
+				envp[b] = envp[b + 1];
+				b++;
+			}
+			return (1);
+		}
+		a++;
+	}
+	printf("minishell: unset: variable %s not found\n", var);
+	return (1);
 }
 
 // Reference:	export(); function form my export.c
